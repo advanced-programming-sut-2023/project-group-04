f8bf7c0f4ce154dc62535ee6a9fecc70933f9d51
AbolfazlCe83 <abolfazlsheikhha833@gmail.com>
2023-04-21 02:20:17 +0330

    Main added!

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.view.LoginMenu;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        LoginMenu loginMenu = new LoginMenu();
+        loginMenu.run();
+    }
+}
