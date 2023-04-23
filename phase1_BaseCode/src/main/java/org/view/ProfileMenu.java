1268078448686ba827595f75e980ff99d3a07af9
keyhan-masoudi <keyhanmasoudi641383@gmail.com>
2023-04-23 07:14:06 +0430

    add ShopMenu(completed)

--- phase1_BaseCode/src/main/java/org/view/ProfileMenu.java
+++ phase1_BaseCode/src/main/java/org/view/ProfileMenu.java
@@ -1,7 +1,5 @@
 package org.view;
 
-import org.model.Player;
-
 import java.util.regex.Matcher;
 
 public class ProfileMenu {
@@ -11,46 +9,43 @@ public class ProfileMenu {
 
         while (true) {
             input = Menu.getScanner().nextLine();
+
             if ((matcher = ProfileCommands.getMatcher(input, ProfileCommands.CHANGE_USERNAME)) != null)
                 System.out.println(Menu.getProfileController().changeUsername(matcher).getMessage());
+
             else if ((matcher = ProfileCommands.getMatcher(input, ProfileCommands.CHANGE_NICKNAME)) != null)
                 System.out.println(Menu.getProfileController().changeNickname(matcher).getMessage());
+
             else if ((matcher = ProfileCommands.getMatcher(input, ProfileCommands.CHANGE_EMAIL)) != null)
                 System.out.println(Menu.getProfileController().changeEmail(matcher).getMessage());
+
             else if ((matcher = ProfileCommands.getMatcher(input, ProfileCommands.CHANGE_SLOGAN)) != null)
                 System.out.println(Menu.getProfileController().changeSlogan(matcher).getMessage());
+
             else if ((matcher = ProfileCommands.getMatcher(input, ProfileCommands.CHANGE_PASSWORD)) != null)
                 System.out.println(Menu.getProfileController().changePassword(matcher).getMessage());
+
             else if (ProfileCommands.getMatcher(input, ProfileCommands.DISPLAY_PROFILE) != null)
                 System.out.println(Menu.getProfileController().displayProfile().getMessage());
+
             else if (ProfileCommands.getMatcher(input, ProfileCommands.REMOVE_SLOGAN) != null)
                 System.out.println(Menu.getProfileController().removeSlogan().getMessage());
+
             else if (ProfileCommands.getMatcher(input, ProfileCommands.DISPLAY_RANK) != null)
                 System.out.println(Menu.getProfileController().showRank().getMessage());
+
             else if (ProfileCommands.getMatcher(input, ProfileCommands.DISPLAY_SLOGAN) != null)
                 System.out.println(Menu.getProfileController().showSlogan().getMessage());
+
             else if (ProfileCommands.getMatcher(input, ProfileCommands.DISPLAY_HIGHSCORE) != null)
                 System.out.println(Menu.getProfileController().highScore().getMessage());
+
             else if (ProfileCommands.getMatcher(input, ProfileCommands.BACK) != null)
                 return;
+
             else
                 System.out.println("Invalid command");
         }
 
-    }
-
-    private String suggestNewUsername(String username) {
-        int counter = 30;
-        while (Player.getPlayerByUsername(username) != null) {
-            username += counter;
-            counter++;
-        }
-        return username;
-    }
-
-    public boolean acceptSuggestedUsername(String suggestedUsername) {
-        System.out.println("this username already exists!\nYou can register with this username: \"" + suggestedUsername + "\"\n" +
-                "type <YES> to accept!");
-        return Menu.getScanner().nextLine().equals("YES");
     }
 }
--- phase1_BaseCode/src/main/java/org/view/ShopMenu.java
+++ phase1_BaseCode/src/main/java/org/view/ShopMenu.java
@@ -1,7 +1,30 @@
 package org.view;
 
+import java.util.regex.Matcher;
+
 public class ShopMenu {
     public void run() {
+        String input;
+        Matcher matcher;
+
+        while (true) {
+            input = Menu.getScanner().nextLine();
+
+            if (ShopCommands.getMatcher(input, ShopCommands.SHOW_PRICE_LIST) != null)
+                System.out.println(Menu.getShopController().showList());
+
+            else if ((matcher = ShopCommands.getMatcher(input, ShopCommands.BUY_ITEM)) != null)
+                System.out.println(Menu.getShopController().buyThing(matcher));
+
+            else if ((matcher = ShopCommands.getMatcher(input, ShopCommands.SELL_ITEM)) != null)
+                System.out.println(Menu.getShopController().sellThing(matcher));
+
+            else if (ShopCommands.getMatcher(input, ShopCommands.BACK) != null)
+                return;
+
+            else
+                System.out.println("Invalid command");
+        }
 
     }
 }
