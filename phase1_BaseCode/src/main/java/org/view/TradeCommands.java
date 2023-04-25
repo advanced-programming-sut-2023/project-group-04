57320579e8349d5d0194b0287c035178acc2ed09
keyhan-masoudi <keyhanmasoudi641383@gmail.com>
2023-04-25 12:24:53 +0430

    complete TradeCommands

--- phase1_BaseCode/src/main/java/org/view/TradeCommands.java
+++ phase1_BaseCode/src/main/java/org/view/TradeCommands.java
@@ -4,9 +4,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public enum TradeCommands {
-    SET_TRADE(""),
+    SET_TRADE("^[ \t]*trade(?=.*-t\\s*(?<type>\"[^\"]*\"|[^\\s\"]*))(?=.*-m\\s*(?<message>\"[^\"]*\"|[^\\s\"]*))" +
+            "(?=.*-p\\s*(?<price>[\\d]*))(?=.*-a\\s*(?<amount>[\\d]*))" +
+            "(\\s*(-t\\s*(\"[^\"]*\"|[^\\s\"]*)|-m\\s*(\"[^\"]*\"|[^\\s\"]*)|-p\\s*([\\d]*)|-a\\s*([\\d]*))){4}[ \t]*$"),
     TRADE_LIST("^[ \t]*trade\\s+list[ \t]*$"),
-    ACCEPT_TRADE(""),
+    ACCEPT_TRADE("^[ \t]*trade\\s+accept(?=.*-m\\s*(?<message>\"[^\"]*\"|[^\\s\"]*))(?=.*-i\\s*(?<id>[\\d]*))" +
+            "(\\s*(-m\\s*(\"[^\"]*\"|[^\\s\"]*)|-i\\s*([\\d]*))){2}[ \t]*$"),
     TRADE_HISTORY("^[ \t]*trade\\s+history[ \t]*$"),
     BACK("^[ \t]*back[ \t]*$");
 
