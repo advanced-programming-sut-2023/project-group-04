16fa3211774d7a42a73148e13d21d45d8a9cd6da
usefsdd <119659668+usefsdd@users.noreply.github.com>
2023-04-25 20:06:58 +0430

    merge code and start of game controller

--- phase1_BaseCode/src/main/java/org/view/CommandsEnum/MapMenuCommands.java
+++ phase1_BaseCode/src/main/java/org/view/CommandsEnum/MapMenuCommands.java
@@ -4,14 +4,21 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public enum MapMenuCommands {
-    CREATE_NEW_MAP("(\\s*)create(\\s+)new(\\s+)map(\\s+)(-i(\\s+)(?<mapId>\\d+))(\\s+)(-s(\\s+)(?<size>\\d+))(\\s*)"),
-    CUSTOM_EXITING_MAP("(\\s*)custom(\\s+)map(\\s+)(-i(\\s+)(?<mapId>\\d+))(\\s*)"),
-    SET_CELL_TEXTURE("(\\s*)set(\\s+)texture(\\s+)(-x(\\s+)(?<x>\\d+))(\\s+)(-y(\\s+)(?<y>\\d+))(\\s+)(-t(\\s+)(?<type>\\S+))(\\s*)"),
-    SET_AREA_TEXTURE("(\\s*)set(\\s+)texture(\\s+)(-x1(\\s+)(?<x1>\\d+))(\\s+)(-y1(\\s+)(?<y1>\\d+))(\\s+)" +
-                           "(-x2(\\s+)(?<x2>\\d+))(\\s+)(-y2(\\s+)(?<y2>\\d+))(\\s+)(-t(\\s+)(?<type>\\S+))(\\s*)"),
-    CLEAR_MAP_CELL("(\\s*)clear(\\s+)(-x(\\s+)(?<x>\\d+))(\\s+)(-y(\\s+)(?<y>\\d+))(\\s*)"),
-    DROP_ROCK("(\\s*)drop(\\s+)rock(\\s+)(-x(\\s+)(?<x>\\d+))(\\s+)(-y(\\s+)(?<y>\\d+))(\\s+)(-d(\\s+)(?<direction>\\w))(\\s*)"),
-    DROP_TREE("(\\s*)drop(\\s+)tree(\\s+)(-x(\\s+)(?<x>\\d+))(\\s+)(-y(\\s+)(?<y>\\d+))(\\s+)(-t(\\s+)(<type>\\S+))(\\s*)"),
+
+    CREATE_NEW_MAP("^\\s*create\\s+new\\s+map(?=.*-i (?<mapId>\\d+))" +
+            "(?=.*-s (?<size>\\d+))(\\s*(-i (\\d+))|(-s (\\d+))){2}\\s*$"),
+    CUSTOM_EXITING_MAP("^\\s*custom\\s+map(?=.*-i (?<mapId>\\d+))(\\s*(-i (\\d+))){1}\\s*$"),
+    SET_CELL_TEXTURE("^\\s*set\\s+texture(?=.*-x (?<x>\\d+))(?=.*-y (?<y>\\d+))(?=.*-t (?<type>\"[^\"]*\"|[^\\s\"]*))" +
+            "(\\s+(-x (\\d+))|(-y (\\d+))|(-t (\"[^\"]*\"|[^\\s\"]*))){3}\\s*$"),
+    SET_AREA_TEXTURE("^\\s*set\\s+texture(?=.*-x1 (?<x1>\\d+))(?=.*-y1 (?<y1>\\d+))(?=.*-x2 (?<x2>\\d+))" +
+            "(?=.*-y2 (?<y2>\\d+))(?=.*-t (?<type>\"[^\"]*\"|[^\\s\"]*))" +
+            "(\\s+(-x1 (\\d+))|(-y1 (\\d+))|(-x2 (\\d+))|(-y2 (\\d+))|(-t (\"[^\"]*\"|[^\\s\"]*))){5}\\s*$"),
+    CLEAR_MAP_CELL("^\\s*clear(?=.*-x (?<x>\\d+))(?=.*-y (?<y>\\d+))" +
+            "(\\s+(-x (\\d+))|(-y (\\d+))){2}\\s*$"),
+    DROP_ROCK("^\\s*drop\\s+rock(?=.*-x (?<x>\\d+))(?=.*-y (?<y>\\d+))(?=.*-d (?<direction>\"[^\"]*\"|[^\\s\"]*))" +
+            "(\\s+(-x (\\d+))|(-y (\\d+))|(-d (\"[^\"]*\"|[^\\s\"]*))){3}\\s*$"),
+    DROP_TREE("^\\s*drop\\s+tree(?=.*-x (?<x>\\d+))(?=.*-y (?<y>\\d+))(?=.*-t (?<type>\"[^\"]*\"|[^\\s\"]*))" +
+            "(\\s+(-x (\\d+))|(-y (\\d+))|(-t (\"[^\"]*\"|[^\\s\"]*))){3}\\s*$"),
     BACK("(\\s*)back(\\s*)");
 
     private String regex;
