997b127963a70bccfc6a856ff0828ff2a2eec4cf
AbolfazlCe83 <abolfazlsheikhha833@gmail.com>
2023-05-05 20:24:07 +0330

    offensivePowers updated!

--- phase1_BaseCode/src/main/java/org/model/person/SoldiersDictionary.java
+++ phase1_BaseCode/src/main/java/org/model/person/SoldiersDictionary.java
@@ -3,24 +3,24 @@ package org.model.person;
 import java.util.HashMap;
 
 public enum SoldiersDictionary {
-    ARCHER("archer", 12,"bow","",0, 0, 0, 16,5),
-    CROSSBOWMAN("crossbowman", 20,"crossBow","leather",0, 0, 0, 8,3),
-    SPEARMAN("spearman", 8,"spear","",0, 0, 0, 12,0),
-    PIKEMAN("pikeman", 20,"pike","metal",0, 0, 0, 8,0),
-    MACEMAN("maceman", 20,"mace","leather",0, 0, 0, 12,0),
-    SWORDSMAN("swordsman", 40,"sword","metal",0, 0, 0, 4,0),
-    KNIGHT("knight", 40,"sword","metal&horse",0, 0, 0, 20,0),
-    TUNNELER("tunneler", 25,"pickAxe","",0, 0, 0, 16,0),
+    ARCHER("archer", 12,"bow","",0, 125, 0, 16,5),
+    CROSSBOWMAN("crossbowman", 20,"crossBow","leather",0, 125, 0, 8,3),
+    SPEARMAN("spearman", 8,"spear","",0, 150, 0, 12,0),
+    PIKEMAN("pikeman", 20,"pike","metal",0, 150, 0, 8,0),
+    MACEMAN("maceman", 20,"mace","leather",0, 175, 0, 12,0),
+    SWORDSMAN("swordsman", 40,"sword","metal",0, 200, 0, 4,0),
+    KNIGHT("knight", 40,"sword","metal&horse",0, 200, 0, 20,0),
+    TUNNELER("tunneler", 25,"pickAxe","",0, 150, 0, 16,0),
     LADDERMAN("ladderman", 5,"","",0, 0, 0, 16,0),
     ENGINEER("engineer", 30,"","",0, 0, 0, 12,0),
-    BLACKMONKS("blackmonks", 10,"staff","",0, 0, 0, 8,0),
-    ARABIANBOWS("arabianbows", 75,"bow","",0, 0, 0, 16,5),
-    SLAVE("slave", 5,"torch","",0, 0, 0, 16,0),
-    SLINGER("slinger", 12,"sling","",0, 0, 0, 16,2),
-    ASSASSIN("assassin", 60,"scimitar","",0, 0, 0, 12,0),
-    HORSE_ARCHER("horse_archer", 80,"bow","horse",0, 0, 0, 20,6),
-    ARABIAN_SWORDSMAN("arabian_swordsman", 80,"scimitar","metalArmour",0, 0, 0, 20,0),
-    FIRE_THROWER("fire_thrower", 100,"greekFire","", 0, 0, 0, 20,8);
+    BLACKMONKS("blackmonks", 10,"staff","",0, 150, 0, 8,0),
+    ARABIANBOWS("arabianbows", 75,"bow","",0, 125, 0, 16,5),
+    SLAVE("slave", 5,"torch","",0, 100, 0, 16,0),
+    SLINGER("slinger", 12,"sling","",0, 125, 0, 16,2),
+    ASSASSIN("assassin", 60,"scimitar","",0, 150, 0, 12,0),
+    HORSE_ARCHER("horse_archer", 80,"bow","horse",0, 125, 0, 20,6),
+    ARABIAN_SWORDSMAN("arabian_swordsman", 80,"scimitar","metalArmour",0, 175, 0, 20,0),
+    FIRE_THROWER("fire_thrower", 100,"greekFire","", 0, 175, 0, 20,8);
 
     int hp;
     private int offensivePower;
