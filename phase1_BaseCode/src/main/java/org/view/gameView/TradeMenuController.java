e7cb148c8984b80b0c8ba079e3a3da8193ffb0ac
AbolfazlCe83 <abolfazlsheikhha833@gmail.com>
2023-07-04 22:47:03 +0330

    initialize trade!

--- phase1_BaseCode/src/main/java/org/view/gameView/TradeMenuController.java
+++ phase1_BaseCode/src/main/java/org/view/gameView/TradeMenuController.java
@@ -1,6 +1,7 @@
 package org.view.gameView;
 
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
@@ -14,6 +15,7 @@ import org.model.Game;
 import org.model.ResourcesDictionary;
 import org.model.*;
 import org.view.CommandsEnum.TradeMessages;
+import org.view.GameMenu;
 import org.view.Menu;
 import org.view.ShopMenu;
 import org.view.TradeMenu;
@@ -110,6 +112,21 @@ public class TradeMenuController {
     public Text id2;
     public Text id1;
 
+
+    @FXML
+    public void initialize() {
+        ArrayList<Empire> opponentEmpires = Game.getCurrentGame().getOpponentEmpires();
+        if (opponentEmpires.size() < 4)
+            player4.setText("");
+        else player4.setText(opponentEmpires.get(3).getOwner().getUsername());
+        if (opponentEmpires.size() < 3)
+            player3.setText("");
+        else player3.setText(opponentEmpires.get(2).getOwner().getUsername());
+        if (opponentEmpires.size() < 2)
+            player2.setText("");
+        else player2.setText(opponentEmpires.get(1).getOwner().getUsername());
+        player1.setText(opponentEmpires.get(0).getOwner().getUsername());
+    }
 
     public void enterShopMenu(MouseEvent mouseEvent) throws Exception {
         new ShopMenu().start(ShopMenu.stage);
